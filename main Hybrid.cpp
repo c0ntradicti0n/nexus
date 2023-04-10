17cb8c246dc033e3d326d7b44c7998cdc91a7997
albisaft <69861682+albisaft@users.noreply.github.com>
2023-04-10 20:12:26 +0200

    230404 FB 6 50
    Bauer auf der sechsten Reihe bringt 50 Punkte.
    
    ELO: + 12

--- main Hybrid.cpp
+++ main Hybrid.cpp
@@ -233,8 +233,7 @@ beginning:
             // wichtige Initkommandos - wo man antworten muss
 
             if (command == "uci") {
-
-                cout << "id name NEXUS 230318 KonKoo 1.3\n";  // 750250 6000
+                cout << "id name NEXUS 230404 FB 6 50\n";  // 750250 6000
                 cout << "id author Albrecht Fiebiger & Stefan Werner\n";
                 cout << "uciok\n"; }
 
